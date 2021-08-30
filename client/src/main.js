a19da58c27cd2163408ee1076d73df4367c1829d
CitrusSilver <sengoku.zange@gmail.com>
2021-08-30 19:15:37 +0800

    Revert "First repo back-up"
    This reverts commit 2420838553abb25080db09b81eb417e4346ca1c3.

--- client/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-import './assets/main.css'
-import './assets/css/darkly_bootstrap.min.css'
-
-createApp(App).use(store).use(router).mount('#app')
