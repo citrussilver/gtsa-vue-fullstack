408dd4fb9952e8d138d017395af1bec9036bdb4b
citrussilver <sengoku.zange@gmail.com>
2025-01-31 10:15:31 +0800

    Apply fix on certain composable fetch api

--- client/src/composables/getAniQuote.js
+++ client/src/composables/getAniQuote.js
@@ -13,11 +13,11 @@ export const generateAniQuote = async () => {
 
     console.log(import.meta.env.VITE_BASE_URL_2);
 
-    // const response = await fetch(`https://proxy.corsfix.com/?${import.meta.env.VITE_BASE_URL_2}/quotes/random`)
-    // const dataObj = await response.json()
-    // const data = dataObj.data
+    const response = await fetch(`https://proxy.corsfix.com/?${import.meta.env.VITE_BASE_URL_2}/quotes/random`)
+    const dataObj = await response.json()
+    const data = dataObj.data
     
-    // aniQuote.quote = data.content
-    // aniQuote.character = data.character.name
-    // aniQuote.anime = data.anime.name;
+    aniQuote.quote = data.content
+    aniQuote.character = data.character.name
+    aniQuote.anime = data.anime.name;
 }
